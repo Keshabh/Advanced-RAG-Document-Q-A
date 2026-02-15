d33d605a3f633250c92a031da7ebb75a0522e673
keshabh <dcquaster79@gmail.com>
2026-02-15 16:04:14 +0530

    Supports Multi-format file processing

--- app/services/pdf_processor.py
+++ app/services/pdf_processor.py
@@ -5,8 +5,90 @@ from services.hashing import hash_text
 from services.chunking import semantic_sections
 from core.vector_store import save_vector_store
 from langchain_community.vectorstores import FAISS
+from docx import Document
+from pptx import Presentation
+import pandas as pd
 
-def processPdf(pdfs, vector_store):
+
+import os
+
+def extract_data(file):
+    ext = os.path.splitext(file.name)[1].lower()
+
+    if ext == ".pdf":
+        return extract_pdf(file)
+
+    elif ext == ".docx":
+        return extract_docx(file)
+
+    elif ext == ".txt":
+        return extract_txt(file)
+
+    elif ext == ".csv":
+        return extract_csv(file)
+
+    elif ext == ".pptx":
+        return extract_pptx(file)
+
+    else:
+        raise ValueError(f"Unsupported file type: {ext}")
+
+def extract_pdf(pdf):
+    reader = PdfReader(pdf)
+    full_text = ""
+    for page in reader.pages:
+        t = page.extract_text()
+        if t:
+            full_text += t + "\n"
+    return full_text
+
+def extract_docx(file):
+    doc = Document(file)
+    full_text = ""
+    
+    for para in doc.paragraphs:
+        if para.text.strip():
+            full_text += para.text + "\n"
+    print(full_text)
+    return full_text
+
+def extract_txt(file):
+    full_text = ""
+    
+    content = file.read().decode("utf-8")
+    if content.strip():
+        full_text += content + "\n"
+    
+    return full_text
+
+def extract_csv(file):
+    full_text = ""
+    
+    df = pd.read_csv(file)
+    
+    for _, row in df.iterrows():
+        row_text = " ".join([str(cell) for cell in row if pd.notna(cell)])
+        if row_text.strip():
+            full_text += row_text + "\n"
+    
+    return full_text
+
+
+def extract_pptx(file):
+    prs = Presentation(file)
+    full_text = ""
+    
+    for slide in prs.slides:
+        for shape in slide.shapes:
+            if hasattr(shape, "text"):
+                text = shape.text.strip()
+                if text:
+                    full_text += text + "\n"
+    
+    return full_text
+
+
+def processFiles(files, vector_store):
     existing_chunk_hashes = set()
     existing_docs = {}
 
@@ -26,17 +108,14 @@ def processPdf(pdfs, vector_store):
     all_metadatas = []
     new_chunk_hashes = set()
 
-    for pdf in pdfs:
-        pdf_bytes = pdf.read()
-        pdf_hash = hash_text(pdf_bytes)
-        pdf.seek(0)
+    for file in files:
+        file_bytes = file.read()
+        file_hash = hash_text(file_bytes)
+        print(file_hash)
+        # exit(1)
+        file.seek(0)
 
-        reader = PdfReader(pdf)
-        full_text = ""
-        for page in reader.pages:
-            t = page.extract_text()
-            if t:
-                full_text += t + "\n"
+        full_text = extract_data(file)
 
         sections = semantic_sections(full_text)
 
@@ -56,9 +135,9 @@ def processPdf(pdfs, vector_store):
 
                 all_texts.append(chunk)
                 all_metadatas.append({
-                    "pdf_hash": pdf_hash,
+                    "file_hash": file_hash,
                     "chunk_hash": chunk_hash,
-                    "source": pdf.name
+                    "source": file.name
                 })
     # st.write(all_texts)
     # exit(1)
@@ -76,8 +155,8 @@ def processPdf(pdfs, vector_store):
 
     # add new chunks
     if all_texts:
-        save_vector_store(vector_store, all_texts, all_metadatas)
-        message = {"success" : "PDF updated incrementally (no duplication)."}
+        vector_store = save_vector_store(vector_store, all_texts, all_metadatas)
+        message = {"success" : "Document updated incrementally (no duplication)."}
     else:
         message = {"info" : "No new content detected."}
 
