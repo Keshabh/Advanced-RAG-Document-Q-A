170894d88d8955316edb4be5c3d167f49202ff66
Keshabh Kedia <55504424+Keshabh@users.noreply.github.com>
2026-05-04 08:11:59 +0530

    Enhance comments in hash_text function
    Add comments to clarify data handling in hash_text function

--- app/services/hashing.py
+++ app/services/hashing.py
@@ -2,6 +2,8 @@ import hashlib
 import re
 
 def hash_text(data):
+    #since file is read and is in bytes format, and hashlib needs byte data to convert it into hash
+    #but chunks are in text format, which is required to converted into bytes first i.e done using encode utf-8
     if isinstance(data, bytes):
         #generate hash for entire file
         return hashlib.sha256(data).hexdigest()
