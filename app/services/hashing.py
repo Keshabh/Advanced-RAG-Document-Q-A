b14639a248fd8a21ac008987ccf3e7655150e56a
Keshabh Kedia <55504424+Keshabh@users.noreply.github.com>
2026-05-04 08:08:22 +0530

    Update comments in hash_text function for clarity

--- app/services/hashing.py
+++ app/services/hashing.py
@@ -3,7 +3,7 @@ import re
 
 def hash_text(data):
     if isinstance(data, bytes):
-        #generate hash for pdf
+        #generate hash for entire file
         return hashlib.sha256(data).hexdigest()
-    #generate hash for text
+    #generate hash for chunk
     return hashlib.sha256(data.encode("utf-8")).hexdigest()
